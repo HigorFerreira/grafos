fbff50f24b8b2cacf5cd844dfb4524d92998fd8c
Higor Ferreira <higor@DESKTOP-KQHKBEA.localdomain>
2018-12-03 23:06:46 +0000

    Starting repo

---
+++ Grafo.h
@@ -0,0 +1,31 @@
+#ifndef GRAFO_H
+#define GRAFO_H
+
+class Grafo
+{
+	private:
+		//int mat[][];
+		int numVertices;
+		//int pos[];
+		
+	public:
+		//static class Aresta{
+		//	private:
+		//		int v1, v2, peso;
+		//	public:
+		//		Aresta(int v1, int v2, int peso){
+		//			this.v1 = v1; this.v2 = v2; this.peso = peso;
+		//		}
+		//		int peso(){
+		//			return this.peso;
+		//		}
+		//		int v1(){
+		//			return this.v1;
+		//		}
+		//		int v2(){
+		//			return this.v2;
+		//		}
+		//};
+};
+
+#endif

6efca21c86a86f2702c9c5f66995167073b1eebb
Higor Ferreira <higor@DESKTOP-KQHKBEA.localdomain>
2018-12-08 16:51:06 +0000

    Problems with classes into classes solved

--- Grafo.h
+++ Grafo.h
@@ -1,31 +1,55 @@
 #ifndef GRAFO_H
 #define GRAFO_H
 
+/*
+class Aresta{
+	int v1, v2, peso;
+	public:
+		Aresta(int v1, int v2, int peso){
+			this->v1 = v1;
+			this->v2 = v2;
+			this->peso = peso;
+		}
+		
+		int peso(){
+			return this->peso;
+		}
+		
+		int v1(){
+			return this->v1;
+		}
+		
+		int v2(){
+			return this->v2;
+		}
+};
+
+class Celula{
+	int vertice, peso;
+	public:
+		Celula(int v, int p){
+			this->vertice = v;
+			this->peso = p;
+		}
+		
+		bool equals (){
+			
+		}
+};
+
+*/
+
 class Grafo
 {
 	private:
-		//int mat[][];
 		int numVertices;
-		//int pos[];
 		
 	public:
-		//static class Aresta{
-		//	private:
-		//		int v1, v2, peso;
-		//	public:
-		//		Aresta(int v1, int v2, int peso){
-		//			this.v1 = v1; this.v2 = v2; this.peso = peso;
-		//		}
-		//		int peso(){
-		//			return this.peso;
-		//		}
-		//		int v1(){
-		//			return this.v1;
-		//		}
-		//		int v2(){
-		//			return this.v2;
-		//		}
-		//};
+		class Aresta{
+			int v1, v2, peso;
+			public:
+				int getPeso(){ return this->peso; }
+		};
 };
 
 #endif

d6a7f4b69de31668795f99e83ec43a3e6d6458b6
Higor Ferreira <higor@DESKTOP-KQHKBEA.localdomain>
2018-12-08 22:27:44 +0000

    Código do livro bugando

--- Grafo.h
+++ Grafo.h
@@ -1,43 +1,9 @@
 #ifndef GRAFO_H
 #define GRAFO_H
 
-/*
-class Aresta{
-	int v1, v2, peso;
-	public:
-		Aresta(int v1, int v2, int peso){
-			this->v1 = v1;
-			this->v2 = v2;
-			this->peso = peso;
-		}
-		
-		int peso(){
-			return this->peso;
-		}
-		
-		int v1(){
-			return this->v1;
-		}
-		
-		int v2(){
-			return this->v2;
-		}
-};
+#include<list>
 
-class Celula{
-	int vertice, peso;
-	public:
-		Celula(int v, int p){
-			this->vertice = v;
-			this->peso = p;
-		}
-		
-		bool equals (){
-			
-		}
-};
-
-*/
+using namespace std;
 
 class Grafo
 {
@@ -63,6 +29,22 @@ class Grafo
 					return (this->vertice == item->vertice);
 				}
 		};
+		
+		list<int> *adj;
+		int numVetices;
+		
+		Grafo(int numVertices){
+			this->adj = new list<Celula>[numVertices];
+			this->numVertices = numVertices;
+			for(int i = 0; i < this->numVertices; i++){
+				this->adj[i] = new list<Celula>;
+			}
+		}
+		
+		void insereAresta(int v1, int v2, int peso){
+			Celula *item = new Celula(v2, peso);
+			this->adj[v1].push_back(item);
+		}
 };
 
 #endif

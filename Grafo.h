2f9ba231ed727fcd6f3c9530b797ca6a5bb4e15d
Higor Ferreira <higor@DESKTOP-KQHKBEA.localdomain>
2018-12-08 23:41:13 +0000

    Criação da matriz de adjacência

--- Grafo.h
+++ Grafo.h
@@ -7,7 +7,22 @@ using namespace std;
 
 class Grafo
 {
-	
+	private:
+		int **matriz;
+	public:
+		Grafo(int numVertices){
+			//Criação da matriz de adjacência
+			this->matriz = new int*[numVertices];
+			for(int i = 0; i < numVertices; i++){
+				this->matriz[i] = new int[numVertices];
+			}
+			//Populando a matriz com 0
+			for(int i; i < numVertices; i++){
+				for(int j; j < numVertices; j++){
+					this->matriz[i][j] = 0;
+				}
+			}
+		}
 };
 
 #endif

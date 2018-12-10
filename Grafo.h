43b78872a9c49add2151b99c4f7f4be5d3113aea
Higor Ferreira <higor@DESKTOP-KQHKBEA.localdomain>
2018-12-10 10:52:43 +0000

    Consertando bug da matriz de adjacência

--- Grafo.h
+++ Grafo.h
@@ -40,7 +40,7 @@ class Grafo
 				this->matriz[i] = new int[numVertices];
 			}
 			//Populando a matriz com 0
-			for(int i; i < numVertices; i++){
+			for(int i = 0; i < numVertices; i++){
 				for(int j; j < numVertices; j++){
 					this->matriz[i][j] = 0;
 				}

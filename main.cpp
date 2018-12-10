82b1d8cead027dd2e65326c0ff1745575cd88a65
Higor Ferreira <higor@DESKTOP-KQHKBEA.localdomain>
2018-12-10 10:32:35 +0000

    Testando a impressão do vetor de tempos --Os tempos não estão sendo impressos

--- main.cpp
+++ main.cpp
@@ -18,4 +18,10 @@ int main(){
 	
 	//Teste de busca em profundidade
 	busca_response *res = Grafo::busca(grafo, 0, 0);
+	
+	//Impressão do vetor de tempos
+	for(int i = 0; i < grafo->getNumVetices(); i++){
+		cout<<res->tempos[i]<<" ";
+	}
+	cout<<endl;
 }
